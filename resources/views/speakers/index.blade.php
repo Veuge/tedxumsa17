c9a7e7101603e8b583dc03fdcb8f104de95957ae
Carlos Zabaleta <zabaletacopa@gmail.com>
2018-03-19 14:24:22 -0400

    Delete unnecessary <br>s

--- resources/views/speakers/index.blade.php
+++ resources/views/speakers/index.blade.php
@@ -66,8 +66,7 @@
                     <h4 class="centre-line">
                         <span>
                             <a href="https://www.youtube.com/playlist?list=PLevEh7kH5s-hGaJJHHdvUsP3yK_xZ27oB" target="_blank">
-                                <!-- Delete the <br> added to look more aesthetically pleasing -->
-+                                Ve <b>aqui</b> las charlas del TEDxUMSA 2017 <br><br><br><br><br><br><br><br><br><br>
++                                Mira <b>aquí</b> las charlas del TEDxUMSA 2017 <br><br>
                             </a>
                         </span>
                     </h4>
